2695bb8424aeb2949f03d24d7ab1ffee39cfc77e
Austin Wood <austin@austinkwood.com>
2017-03-14 12:10:54 -0700

    Render root in page

---
+++ frontend/entry.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from './store/store.js';
+// import Root from './components/root';
+
+// import { login, signup, logout } from './actions/session_actions';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>Yo ho!</h1>, root);
+});
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const store = configureStore();
+//   const root = document.getElementById('root');
+//   ReactDOM.render(<Root store={ store }/>, root);
+//   // window.store = store;
+//   // window.login = login;
+// });
