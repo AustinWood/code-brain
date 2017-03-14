8bc24dd7ba1ad9b59d78b2ec2c3a56c8d18701a6
Austin Wood <austin@austinkwood.com>
2017-03-14 13:45:02 -0700

    Create User model

---
+++ test/models/user_test.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string
+#  password_digest :string
+#  session_token   :string
+#  user_id         :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
