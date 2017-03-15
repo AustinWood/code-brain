658c76880b52d76761620db0aa3586498dda0965
Austin Wood <austin@austinkwood.com>
2017-03-15 11:47:57 -0700

    Finish styling nav bar

--- frontend/components/nav_bar.jsx
+++ frontend/components/nav_bar.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
 
+
+
 const NavBar = ({ currentUser, logout }) => {
   if (!currentUser) {
     return (
       <div></div>
     );
   }
+
   return (
     <div className="nav-bar">
       <div className="nav-left">
-        <img src={"/assets/settings.png"} className="nav-img" />
-        <button onClick={logout}>
-          <img src={"/assets/user_outline_light.png"} className="nav-img" />
-        </button>
+        <img src={"/assets/list_outline.png"} className="nav-img" />
+        <img src={"/assets/add_outline.png"} className="nav-img" />
       </div>
       <div className="nav-center">
-        <img src={"/assets/settings.png"} className="nav-img" />
-        <button onClick={logout}>
-          <img src={"/assets/user_outline_light.png"} className="nav-img" />
-        </button>
+        <img src={"/assets/play_outline.png"} className="play-img" />
+        <p className="due-text">25 notes due</p>
       </div>
       <div className="nav-right">
         <img src={"/assets/settings.png"} className="nav-img" />
