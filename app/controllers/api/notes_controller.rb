c1f276dd684d464549be666275656dd758d6b091
Austin Wood <austin@austinkwood.com>
2017-03-16 17:57:20 -0700

    Begin setting up notes controller

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -1,2 +1,32 @@
 class Api::NotesController < ApplicationController
+  def index
+
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def create
+    @note = Note.create!(note_params)
+    render :show
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def note_params
+    params.require(:note).permit(
+    :question,
+    :skeleton,
+    :answer,
+    :author_id
+    )
+  end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -12,4 +12,5 @@
 #
 
 class Note < ApplicationRecord
+  
 end
