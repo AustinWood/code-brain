d45aca094a2ec7d8a078ebf173e4d45d5a1f2f34
Austin Wood <austin@austinkwood.com>
2017-03-17 07:06:49 -0700

    Create note seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+demo = User.create!(
+  username: "demo_account",
+  password: "password"
+)
+
+notes = [
+  [
+    "You have array of integers. Write a recursive solution to find the sum of the integers.",
+    ".",
+    ".",
+  ],
+  [
+    "You have an unsorted array of integers. Write a recursive solution to count the number of occurrences of a specific value.",
+    ".",
+    ".",
+  ],
+  [
+    "Write a method that squares each element in an array",
+    ".",
+    ".",
+  ],
+  [
+    "Rewrite Ruby's Array#each method.",
+    ".",
+    ".",
+  ],
+  [
+    "Rewrite Ruby's Array#uniq method.",
+    ".",
+    ".",
+  ],
+  [
+    "Rewrite Ruby's Array#select method.",
+    ".",
+    ".",
+  ]
+]
+
+notes.each do |note|
+  Note.create!(
+    question: note[0],
+    skeleton: note[1],
+    answer: note[2],
+    author_id: 1
+  )
+end
+
+# {
+#   question: ".",
+#   skeleton: ".",
+#   answer: ".",
+#   author_id: 1
+# },
