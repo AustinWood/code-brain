8ded7612757bef532eeb60e8fd415617525bd2c4
Austin Wood <austin@austinkwood.com>
2017-03-19 14:48:09 -0700

    Restore notes index header

--- frontend/components/search/notes_index.jsx
+++ frontend/components/search/notes_index.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import NotesIndexItem from './notes_index_item';
-// <p className="notes-index-header">Showing {notes.length} of {notes.length} notes</p>
 
-// TODO: Refactor
-const noteComponents = (notes = [], selectNote, selectedNoteId) => {
-  console.log(`${selectedNoteId}`);
-  return (
-    notes.map(note => (
-
-      <NotesIndexItem
-        note={note}
-        key={note.id}
-        selectNote={selectNote}
-        selected={note.id === selectedNoteId} />
-
-    ))
-  );
-};
+const noteComponents = (notes = [], selectNote, selectedNoteId) => (
+  notes.map(note => (
+    <NotesIndexItem
+      note={note}
+      key={note.id}
+      selectNote={selectNote}
+      selected={note.id === selectedNoteId} />
+  ))
+);
 
 const NotesIndex = ({ notes, selectNote, selectedNoteId }) => (
   <div className="notes-index-component">
+    <p className="notes-index-header">Showing {notes.length} of {notes.length} notes</p>
+
     <div className="notes-index-container">
       {noteComponents(notes, selectNote, selectedNoteId)}
     </div>
+    
   </div>
 );
 
