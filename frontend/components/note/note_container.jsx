4b35b02c18fec7f0da044f123de690e6fc186f1e
Austin Wood <austin@austinkwood.com>
2017-03-19 06:53:16 -0700

    Fix note_container

--- frontend/components/note/note_container.jsx
+++ frontend/components/note/note_container.jsx
@@ -1,22 +1,15 @@
-  import Note from './note';
 import { connect } from 'react-redux';
-// import { fetchNotes, fetchNote } from '../../actions/note_actions';
 
-// const demoNote = {
-//   question: "You have an array of integers. Write a recursive solution to find the sum of the integers.",
-//   skeleton: `{"entityMap":{},"blocks":[{"key":"du6sp","text":"function sumRecur(array) {","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"33gec","text":"  if (array.length === 0) return 0;","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"1b9jr","text":"  if (array.length === 1) return array[0];","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"6tcg6","text":"  let sum = sumRecur(array.slice(1));","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"1n9pc","text":"  return sum + array[0];","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"e8q8u","text":"}","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}]}`,
-//   solution: `{"entityMap":{},"blocks":[{"key":"du6sp","text":"function sumRecur(array) {","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"33gec","text":"  if (array.length === 0) return 0;","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"1b9jr","text":"  if (array.length === 1) return array[0];","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"6tcg6","text":"  let sum = sumRecur(array.slice(1));","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"1n9pc","text":"  return sum + array[0];","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"e8q8u","text":"}","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}]}`
-// };
+import Note from './note';
+import { fetchNote, updateNote, deleteNote } from '../../actions/note_actions';
 
-const mapStateToProps = ({ display }) => ({
-  note: display.note
+const mapStateToProps = (state, { params }) => ({
+  note: state.display.note
 });
 
 const mapDispatchToProps = dispatch => ({
-  // requestNotes: () => dispatch(fetchNotes())
-  // ,
-  // createTodo: todo => dispatch(createTodo(todo)),
-  // updateTodo: todo => dispatch(updateTodo(todo))
+  updateNote: note => dispatch(updateNote(note)),
+  deleteNote: id => dispatch(deleteNote(id))
 });
 
 export default connect(
