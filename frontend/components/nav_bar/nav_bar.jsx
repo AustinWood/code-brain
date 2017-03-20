54c3523976802f4cdec53d6980355f68e9baa95f
Austin Wood <austin@austinkwood.com>
2017-03-20 07:55:54 -0700

    Add hover effects to images

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -14,22 +14,22 @@ class NavBar extends React.Component {
 
         <div className="nav-left">
           <button onClick={this.props.toggleSearch}>
-            <img src={"http://res.cloudinary.com/oblaka/image/upload/v1489617054/list_htttpj.png"} className="nav-img" />
+            <img src={"http://res.cloudinary.com/oblaka/image/upload/v1489617054/list_htttpj.png"} className="nav-img" id="list-img" />
           </button>
           <button onClick={this.props.addNote}>
-            <img src={"http://res.cloudinary.com/oblaka/image/upload/v1489617054/add_afa7gj.png"} className="nav-img" />
+            <img src={"http://res.cloudinary.com/oblaka/image/upload/v1489617054/add_afa7gj.png"} className="nav-img" id="add-img" />
           </button>
         </div>
 
         <div className="nav-center" onClick={this.props.study}>
-          <img src={"http://res.cloudinary.com/oblaka/image/upload/v1489617054/play_wy8ebz.png"} className="play-img" />
+          <img src={"http://res.cloudinary.com/oblaka/image/upload/v1489617054/play_wy8ebz.png"} id="play-img" />
           <p className="due-text">{randomNum} notes due</p>
         </div>
 
         <div className="nav-right">
-          <img src={"http://res.cloudinary.com/oblaka/image/upload/v1489617054/settings_k4rizi.png"} className="nav-img" />
+          <img src={"http://res.cloudinary.com/oblaka/image/upload/v1489617054/settings_k4rizi.png"} className="nav-img" id="settings-img" />
           <button onClick={this.props.logout}>
-            <img src={"http://res.cloudinary.com/oblaka/image/upload/v1489617054/user_dnmke9.png"} className="nav-img" />
+            <img src={"http://res.cloudinary.com/oblaka/image/upload/v1489617054/user_dnmke9.png"} className="nav-img" id="user-img" />
           </button>
         </div>
 
