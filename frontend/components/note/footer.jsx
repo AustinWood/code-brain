15953a3f2f316e621054bb2fc9437934c1d22c67
Austin Wood <austin@austinkwood.com>
2017-03-21 07:25:30 -0700

    Restore delete functionality

--- frontend/components/note/footer.jsx
+++ frontend/components/note/footer.jsx
@@ -10,7 +10,7 @@ class Footer extends React.Component {
   }
 
   handleDelete() {
-    this.props.deleteNote(this.props.noteId);
+    this.props.deleteNote(this.props.note.id);
   }
 
   handleSave() {
