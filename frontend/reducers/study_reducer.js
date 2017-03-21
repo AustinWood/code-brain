645f7b94622c1c80abd65ae59cf8048dbb531df7
Austin Wood <austin@austinkwood.com>
2017-03-21 10:55:54 -0700

    Create arr of note ids in study reducer

--- frontend/reducers/study_reducer.js
+++ frontend/reducers/study_reducer.js
@@ -1,6 +1,5 @@
-// import {
-//   ADD_NOTE, SELECT_NOTE, TOGGLE_SEARCH,
-//   STUDY, REVEAL_ANSWER, NEXT_QUESTION } from '../actions/navigation_actions';
+import { STUDY, REVEAL_ANSWER, NEXT_QUESTION } from '../actions/navigation_actions';
+import { RECEIVE_NOTES } from '../actions/note_actions';
 import merge from 'lodash/merge';
 
 const _study = Object.freeze({
@@ -11,12 +10,28 @@ const NavigationReducer = (state = _study, action) => {
   Object.freeze(state);
   let newState;
   switch(action.type) {
-    // case ADD_NOTE:
-    //   newState = {
-    //     showSearch: true,
-    //     footerType: 'edit'
-    //   };
-    //   return merge({}, state, newState);
+    case RECEIVE_NOTES:
+      const idStrs = Object.keys(action.notes);
+      const idInts = idStrs.map(el => parseInt(el, 10));
+      return {dueNotes: idInts};
+    case STUDY:
+      // grab all note id's and shuffle
+      newState = {
+        // dueNotes: []
+      };
+      return merge({}, state, newState);
+    case REVEAL_ANSWER:
+      // i dont't think this reducer should do anything for this action..?
+      newState = {
+        // dueNotes: []
+      };
+      return merge({}, state, newState);
+    case NEXT_QUESTION:
+      // POP OFF THE FIRST note
+      newState = {
+        // dueNotes: []
+      };
+      return merge({}, state, newState);
     default:
       return state;
   }
