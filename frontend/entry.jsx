84377c1516df412776517de933786bb49980c3f8
Austin Wood <austin@austinkwood.com>
2017-07-14 13:03:11 -0700

    Remove dynoPinger

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -17,17 +17,5 @@ document.addEventListener('DOMContentLoaded', () => {
   window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
-  dynoPinger();
   window.fetchNotes = fetchNotes;
 });
-
-let dynoPinger = () => {
-  var http = require("http");
-  setInterval(function() {
-    const currentdate = new Date();
-    const hours = currentdate.getHours();
-    if (hours > 5 && hours < 22) {
-      http.get("https://codebrain.herokuapp.com");
-    }
-  }, 900000); // every 15 minutes
-};
